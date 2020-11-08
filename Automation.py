24770087eda1af6d554af082d3313aedd73af8cd
Shaurya2007 <57561094+Shaurya2007@users.noreply.github.com>
2020-11-08 11:19:29 +0530

    Add files via upload

---
+++ Automation.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+
+#You can change src and dest if you want
+src = "C:/Nothing"
+dest = "D:/Here"
+
+# Uncomment a line to make the corresponding arguement work!
+
+#For copying all files
+#shutil.copytree(src=src,dst=dest)
+
+#For copying a file
+#shutil.copy(src=src + "/hi.txt",dst=dest)
+
+#For moving a file
+#shutil.move(src=src,"/hey",dst=dest)s
